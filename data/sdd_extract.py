702c7f9cc142c8225abec2cd316ce826c3ab07fa
Paul Féry <paulfery4@gmail.com>
2023-05-03 17:19:25 +0200

    create sdd_extract, to extract and preprocess the SDD annotation data

---
+++ data/sdd_extract.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import yaml
+
+SDD_COL_NAMES = ["Id", "xmin", "ymin", "xmax", "ymax", "frame", "lost", "occl.", "gen.", "label"]
+
+
+def get_config() -> dict:
+    """
+    reads the config file 'config.yaml' at the root of the project repository
+    :return: the contents of the config file, as a dict
+    """
+    # read config file
+    confpath = os.path.abspath(os.path.realpath(__file__) + "/../../config.yaml")
+    assert os.path.exists(confpath), f"ERROR | PATH DOES NOT EXIST:\n{confpath}"
+    # print(f"Loading config from:\n{confpath}\n")
+
+    with open(confpath, "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            # print(config)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+
+def pd_df_from(annotation_filepath):
+    """
+    takes the path of an annotation.txt file of the SDD, and produces its corresponding pandas dataframe
+    :param annotation_filepath: the path of the annotation file
+    :return: a pandas dataframe
+    """
+    return pd.read_csv(annotation_filepath, sep=" ", names=SDD_COL_NAMES)
+
+
+def add_xy_columns_to(annot_df):
+    annot_df["x"] = (annot_df["xmin"] + annot_df["xmax"]) / 2
+    annot_df["y"] = (annot_df["ymin"] + annot_df["ymax"]) / 2
+
+
+if __name__ == '__main__':
+    print(get_config())
