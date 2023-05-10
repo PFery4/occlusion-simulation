d87508837c1c8934eff2abb0cac56b58ac66d2dd
Paul Féry <paulfery4@gmail.com>
2023-05-10 21:11:37 +0200

    WIP dataloader: needs pasts/futures

--- data/sdd_dataloader.py
+++ data/sdd_dataloader.py
@@ -1,37 +1,113 @@
 import os.path
-
-import torch
+import pandas as pd
 from torch.utils.data import Dataset
 import sdd_extract
 import sdd_data_processing
-
+import numpy as np
+import cv2
+import torchvision.transforms
 
 class StanfordDroneDataset(Dataset):
 
     def __init__(self, config):
         self.root = config["dataset"]["path"]
+        self.orig_fps = config["dataset"]["fps"]
+        self.fps = config["hyperparameters"]["fps"]
+        self.T_obs = config["hyperparameters"]["T_obs"]
+        self.T_pred = config["hyperparameters"]["T_pred"]
+
+        # to convert cv2 image to torch tensor
+        self.img_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+
+        frames = []
+        scene_keys = []
+
+        # the lookuptable is a dataframe separate from the dataframe containing all trajectory data.
+        # each row fully describes a complete training instance, with its corresponding video/scene name, and timestep.
+        self.lookuptable = pd.DataFrame(columns=["scene/video", "timestep"])
 
         # read the csv's for all videos
         # hopefully it is manageable within memory
         for scene_name in os.scandir(os.path.join(self.root, "annotations")):
             for video_name in os.scandir(os.path.realpath(scene_name)):
+                scene_key = f"{scene_name.name}/{video_name.name}"
                 annot_file_path = os.path.join(os.path.realpath(video_name), "annotations.txt")
                 print(annot_file_path)
                 assert os.path.exists(annot_file_path)
 
+                annot_df = sdd_extract.pd_df_from(annotation_filepath=annot_file_path)
+
+                # perform preprocessing steps when reading the data
+                annot_df = sdd_data_processing.bool_columns_in(annot_df)
+                annot_df = sdd_data_processing.completely_lost_trajs_removed_from(annot_df)
+                annot_df = sdd_data_processing.xy_columns_in(annot_df)
+                annot_df = sdd_data_processing.keep_masks_in(annot_df)
+                annot_df = annot_df[annot_df["keep"]]
+                annot_df = sdd_data_processing.subsample_timesteps_from(
+                    annot_df, target_fps=self.fps, orig_fps=self.orig_fps
+                )
+                # annot_df["scene_name"] = scene_name.name
+                # annot_df["video_name"] = video_name.name
+                # annot_df["window"] = False
+                # annot_df["scene/video"] = f"{scene_name.name}/{video_name.name}"
+
+                # print(annot_df[["Id", "frame"]])
+                # print(scene_name.name, video_name.name)
+                timesteps = sorted(annot_df["frame"].unique())
+                # print(timesteps)
+                # assert (np.array(timesteps[:-1]) + int(self.orig_fps//self.fps) == np.array(timesteps[1:])).all()
+
+                # We are interested in time windows of [-T_obs : T_pred].
+                # But only windows which contain at least one agent for us to be able to predict, otherwise there's
+                # nothing useful in a training instance.
+                for idx, timestep in enumerate(timesteps[:-(self.T_obs + self.T_pred - 1)]):
+                    window = np.array(timesteps[idx: idx + self.T_obs + self.T_pred - 1])
+                    # print(idx, window)
+
+                    # some timesteps are inexistant in the dataframe, due to the absence of any annotations at those
+                    # points in time. we check that we can have a complete window. (note that some agents might not have
+                    # all their timesteps present within the window)
+                    if (window[:-1] + int(self.orig_fps//self.fps) == window[1:]).all():
+                        self.lookuptable.loc[len(self.lookuptable)] = {"scene/video": scene_key, "timestep": timestep}
+                        # self.lookuptable.append({"scene/video": scene_key, "timestep": timestep}, ignore_index=True)
+                        # annot_df.loc[annot_df["frame"] == timestep, ["window"]] = True
+                    # else:
+                    #     print(f"FOUND WRONG WINDOW; {idx} ; {window}")
+
+                frames.append(annot_df)
+                scene_keys.append(scene_key)
+
+        self.frames = pd.concat(frames, keys=scene_keys)
+
     def __len__(self):
-        pass
+        return len(self.lookuptable)
 
     def __getitem__(self, idx):
-        pass
+        # lookup the row in self.lookuptable
+        lookup = self.lookuptable.iloc[idx]
 
-    def all_subfolders(self):
-        print(os.path.join(self.root, "annotations"))
+        # extract the reference image
+        scene, video = lookup["scene/video"].split("/", 1)
+        image_path = os.path.join(self.root, "annotations", scene, video, "reference.jpg")
+        assert os.path.exists(image_path)
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_tensor = self.img_transform(image)
 
-        for scene_name in os.scandir(os.path.join(self.root, "annotations")):
-            print(scene_name.path)
-            print(sorted(os.listdir(scene_name.path)))
-        return sorted(os.listdir(os.path.join(self.root, "annotations")))
+        # extract the past and futures of present agents at the window starting at the given timestep
+
+        # TODO: FINISH HERE, EXTRACT PASTS AND FUTURES
+
+        window = np.arange(self.T_obs + self.T_pred) * int(self.orig_fps // self.fps) + lookup["timestep"]
+        print(window)
+
+        small_df = self.frames.loc[lookup["scene/video"]][self.frames.loc[lookup["scene/video"]]["frame"].isin(window)]
+        print(len(small_df), "hey")
+
+        for agent_id in small_df["Id"].unique():
+            print(agent_id)
+
+        return image_tensor
 
 
 if __name__ == '__main__':
@@ -39,4 +115,10 @@ if __name__ == '__main__':
     config = sdd_extract.get_config()
 
     dataset = StanfordDroneDataset(config=config)
-    print(dataset.all_subfolders())
+
+    print(dataset.frames)
+    print(dataset.lookuptable)
+    print(len(dataset))
+    dataset.__getitem__(0)
+    dataset.__getitem__(2)
+    dataset.__getitem__(-1)
