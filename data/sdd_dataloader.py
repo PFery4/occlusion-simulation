d246223d053ce908705e658cf66c7c8bf53d82a1
Paul Féry <paulfery4@gmail.com>
2023-05-10 11:34:13 +0200

    WIP dataloader: access all annotation files from dataset class

--- data/sdd_dataloader.py
+++ data/sdd_dataloader.py
@@ -3,12 +3,21 @@ import os.path
 import torch
 from torch.utils.data import Dataset
 import sdd_extract
+import sdd_data_processing
 
 
 class StanfordDroneDataset(Dataset):
 
     def __init__(self, config):
         self.root = config["dataset"]["path"]
+
+        # read the csv's for all videos
+        # hopefully it is manageable within memory
+        for scene_name in os.scandir(os.path.join(self.root, "annotations")):
+            for video_name in os.scandir(os.path.realpath(scene_name)):
+                annot_file_path = os.path.join(os.path.realpath(video_name), "annotations.txt")
+                print(annot_file_path)
+                assert os.path.exists(annot_file_path)
 
     def __len__(self):
         pass
@@ -23,7 +32,6 @@ class StanfordDroneDataset(Dataset):
             print(scene_name.path)
             print(sorted(os.listdir(scene_name.path)))
         return sorted(os.listdir(os.path.join(self.root, "annotations")))
-
 
 
 if __name__ == '__main__':
