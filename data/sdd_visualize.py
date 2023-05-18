e35ff465c439192d3725546c329d1f9b08f09973
Paul Féry <paulfery4@gmail.com>
2023-05-18 12:42:17 +0200

    minor cleanups + sdd_visualize calls preprocessing pipeline function

--- data/sdd_visualize.py
+++ data/sdd_visualize.py
@@ -24,12 +24,12 @@ def count_timesteps(annot_df: pd.DataFrame):
     """
     frames = annot_df["frame"].unique()
     n_timesteps = max(frames) - min(frames) + 1
-    # len(annot_df["frame"].unique())
     return n_timesteps
 
 
 def timesteps_to_seconds(n, fps=30):
     return n/fps
+
 
 def draw_single_trajectory_onto_image(draw_ax: matplotlib.axes.Axes, agent_df: pd.DataFrame, c):
     agent_class = agent_df["label"].iloc[0]
@@ -70,9 +70,6 @@ def draw_all_trajectories_onto_image(draw_ax: matplotlib.axes.Axes, traj_df: pd.
     for agent in agents:
         agent_df = traj_df[traj_df["Id"] == agent]
         c = next(color_iter).reshape(1, -1)
-
-        sdd_data_processing.get_keep_mask_from(agent_df)
-
         draw_single_trajectory_onto_image(draw_ax=draw_ax, agent_df=agent_df, c=c)
 
 
@@ -108,6 +105,7 @@ def visualize_training_instance(draw_ax: matplotlib.axes.Axes, **kwargs):
             draw_ax.plot(future[:, 0], future[:, 1], c=c, alpha=0.7)
             draw_ax.scatter(future[:, 0], future[:, 1], c=c, marker="x", alpha=0.7)
 
+
 def visualize_full_trajectories_on_all_scenes():
     config_dict = sdd_extract.get_config()
     data_path = config_dict["dataset"]["path"]
@@ -131,12 +129,12 @@ def visualize_full_trajectories_on_all_scenes():
             # mins, sec = duration // 60, duration % 60
             # print(f"{scene_name.name}, {video_name.name}: {n_timesteps} timesteps ({duration}s: {mins}m, {sec}s)")
 
-            annot_file_df = sdd_data_processing.bool_columns_in(annot_file_df)
-            annot_file_df = sdd_data_processing.completely_lost_trajs_removed_from(annot_file_df)
-            annot_file_df = sdd_data_processing.xy_columns_in(annot_file_df)
-            annot_file_df = sdd_data_processing.keep_masks_in(annot_file_df)
-            annot_file_df = annot_file_df[annot_file_df["keep"]]
-            annot_file_df = sdd_data_processing.subsample_timesteps_from(annot_file_df)
+            annot_file_df = sdd_data_processing.perform_preprocessing_pipeline(
+                annot_df=annot_file_df,
+                agent_types=config_dict["hyperparameters"]["agent_types"],
+                target_fps=config_dict["hyperparameters"]["fps"],
+                orig_fps=config_dict["dataset"]["fps"]
+            )
 
             # with pd.option_context('display.max_rows', None,
             #                        'display.max_columns', None,
