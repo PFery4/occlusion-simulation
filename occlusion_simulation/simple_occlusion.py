3537c21dedd93860418da85262f03d409669360e
Paul Féry <paulfery4@gmail.com>
2023-05-25 12:12:07 +0200

    wip occlusion simulation

--- occlusion_simulation/simple_occlusion.py
+++ occlusion_simulation/simple_occlusion.py
@@ -3,6 +3,24 @@ import data.sdd_dataloader as sdd_dataloader
 import data.sdd_extract as sdd_extract
 import data.sdd_visualize as sdd_visualize
 
+
+def place_ego(instance_dict: dict):
+
+    agent_id = 4
+
+    past = instance_dict["pasts"][agent_id]
+    future = instance_dict["futures"][agent_id]
+    label = instance_dict["labels"][agent_id]
+    full_ons = instance_dict["is_fully_observed"][agent_id]
+
+    for k, v in instance_dict.items():
+        print(f"{k}\t\t{v}")
+
+
+def place_occluder(instance_dict: dict):
+    pass
+
+
 if __name__ == '__main__':
     print("Ok, let's do this")
 
@@ -16,6 +34,8 @@ if __name__ == '__main__':
     fig, ax = plt.subplots()
 
     sdd_visualize.visualize_training_instance(ax, instance_dict=instance_dict)
+
+    place_ego(instance_dict=instance_dict)
 
     plt.show()
 
