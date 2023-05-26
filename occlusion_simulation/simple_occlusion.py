9b67e189de365d0f586615e2424819990a98c072
Paul Féry <paulfery4@gmail.com>
2023-05-26 09:16:24 +0200

    wip simple occlusion

--- occlusion_simulation/simple_occlusion.py
+++ occlusion_simulation/simple_occlusion.py
@@ -1,20 +1,60 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import data.sdd_dataloader as sdd_dataloader
 import data.sdd_extract as sdd_extract
 import data.sdd_visualize as sdd_visualize
 
 
+def point_between(point_1: np.array, point_2: np.array, k):
+    # k between 0 and 1, point_1 and point_2 of same shape
+    return k * point_1 + (1 - k) * point_2
+
+
 def place_ego(instance_dict: dict):
 
-    agent_id = 4
+    agent_id = 12
+    idx = instance_dict["agent_ids"].index(agent_id)
+    past = instance_dict["pasts"][idx]
+    future = instance_dict["futures"][idx]
+    label = instance_dict["labels"][idx]
+    full_ons = instance_dict["is_fully_observed"][idx]
 
-    past = instance_dict["pasts"][agent_id]
-    future = instance_dict["futures"][agent_id]
-    label = instance_dict["labels"][agent_id]
-    full_ons = instance_dict["is_fully_observed"][agent_id]
+    fig, ax = plt.subplots()
+    sdd_visualize.visualize_training_instance(ax, instance_dict=instance_dict)
 
-    for k, v in instance_dict.items():
-        print(f"{k}\t\t{v}")
+    # pick random past point
+    min_obs = 2     # minimum amount of timesteps we want to have observed within observation window
+    T_occl = np.random.randint(0, past.shape[0] - min_obs) # number of timesteps to move to the past from t_0 (i.e., 0 -> t_0, 1 -> t_-1, etc)
+    past_idx = -1-T_occl
+
+    ax.scatter(past[past_idx][0], past[past_idx][1], marker="x", c="red")
+
+    # pick random future point
+    min_reobs = 2   # minimum amount of timesteps we want to be able to reobserve after disocclusion
+    T_disoccl = future_idx = np.random.randint(0, future.shape[0] - min_reobs)
+
+    ax.scatter(future[future_idx][0], future[future_idx][1], marker="x", c="orange")
+
+    print(f"{T_occl=}")
+    print(f"{T_disoccl=}")
+
+    p_occl = point_between(past[past_idx], past[past_idx-1], np.random.random())
+    p_disoccl = point_between(future[future_idx], future[future_idx+1], np.random.random())
+
+    ax.scatter(p_occl[0], p_occl[1], marker="x", c="yellow")
+    ax.scatter(p_disoccl[0], p_disoccl[1], marker="x", c="green")
+
+    midpoint = point_between(p_occl, p_disoccl, 0.5)
+
+    ax.scatter(midpoint[0], midpoint[1], marker="x", c="blue")
+
+
+
+    # for i in range(100):
+    #     print(random_point_between(past, future))
+
+    plt.show()
+
 
 
 def place_occluder(instance_dict: dict):
@@ -31,11 +71,6 @@ if __name__ == '__main__':
 
     instance_dict = dataset.__getitem__(instance_idx)
 
-    fig, ax = plt.subplots()
-
-    sdd_visualize.visualize_training_instance(ax, instance_dict=instance_dict)
-
     place_ego(instance_dict=instance_dict)
 
-    plt.show()
 
