fa456d604e956e83d049dee8f6892f9945938623
Paul Féry <paulfery4@gmail.com>
2023-05-28 19:49:51 +0200

    code cleanup on simulation

--- occlusion_simulation/simple_occlusion.py
+++ occlusion_simulation/simple_occlusion.py
@@ -2,6 +2,8 @@ import matplotlib.axes
 import matplotlib.pyplot as plt
 import numpy as np
 import shapely.ops
+import torch
+
 import data.sdd_dataloader as sdd_dataloader
 import data.sdd_extract as sdd_extract
 import data.sdd_visualize as sdd_visualize
@@ -10,7 +12,7 @@ import matplotlib.patches as mpl_patches
 import matplotlib.collections as mpl_coll
 from shapely.geometry import LineString, Polygon, GeometryCollection
 from shapely.ops import unary_union, triangulate
-from typing import List
+from typing import List, Tuple
 
 
 def point_between(point_1: np.array, point_2: np.array, k: float) -> np.array:
@@ -23,11 +25,11 @@ def rotation_matrix(theta: float) -> np.array:
     return np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
 
-def no_ego_cone(p: np.array, u: np.array, theta: float, frame_box: Polygon) -> Polygon:
+def bounded_wedge(p: np.array, u: np.array, theta: float, frame_box: Polygon) -> Polygon:
     """
     generates the polygon corresponding to a section of the plane bounded by:
-    - two lines, intersecting at point p, such that their bisector points towards unit vector u. the angle of the lines
-    with respect to the bisector is equal to theta.
+    - a wedge described by two lines, intersecting at point p, such that their bisector points towards unit vector u.
+    the angle of the lines with respect to the bisector is equal to theta.
     - frame_box, a polygon which contains point p.
     """
     big_vec = -u * 2 * np.max(frame_box.bounds)        # large vector, to guarantee cone is equivalent to infinite
@@ -78,99 +80,109 @@ def random_points_in_triangles_collection(triangles: List[Polygon], k: int) -> n
     return points
 
 
+def random_interpolated_point(traj_seq: np.array, timestep_bounds: Tuple[int, int]):
+    """
+    sample a random point among the interpolated sequence of coordinates traj_seq. the point will lie within the
+    interval specified by timestep_bounds (both inclusive)
+    :param traj_seq: shape [Timesteps, Dimensions]
+    :param timestep_bounds: tuple (begin, end)
+    """
+    t_idx = np.random.randint(timestep_bounds[0], timestep_bounds[1]-1)
+    return point_between(traj_seq[t_idx], traj_seq[t_idx+1], np.random.random())
+
+
+def rectangle(image_tensor: torch.Tensor) -> Polygon:
+    # returns a rectangle with dimensions corresponding to those of the input image_tensor
+    y_img, x_img = image_tensor.shape[1:]
+    return Polygon([(0, 0), (x_img, 0), (x_img, y_img), (0, y_img), (0, 0)])
+
+
+def extruded_polygon(polygon: Polygon, d_border: float) -> Polygon:
+    hole = polygon.buffer(-d_border)
+    return Polygon(shell=polygon.exterior.coords, holes=[hole.exterior.coords])
+
+
 def place_ego(instance_dict: dict):
     agent_id = 12
     idx = instance_dict["agent_ids"].index(agent_id)
     past = instance_dict["pasts"][idx]
     future = instance_dict["futures"][idx]
-    label = instance_dict["labels"][idx]
-    full_obs = instance_dict["is_fully_observed"][idx]
 
     fig, ax = plt.subplots()
     sdd_visualize.visualize_training_instance(ax, instance_dict=instance_dict)
 
-    # pick random past point
     min_obs = 2     # minimum amount of timesteps we want to have observed within observation window
-    T_occl = np.random.randint(0, past.shape[0] - min_obs) # number of timesteps to move to the past from t_0 (i.e., 0 -> t_0, 1 -> t_-1, etc)
-    past_idx = -1-T_occl
-
-    # pick random future point
     min_reobs = 2   # minimum amount of timesteps we want to be able to reobserve after disocclusion
-    T_disoccl = future_idx = np.random.randint(0, future.shape[0] - min_reobs)
 
-    # pick random points for start and end of occlusion region
-    p_occl = point_between(past[past_idx], past[past_idx-1], np.random.random())
-    p_disoccl = point_between(future[future_idx], future[future_idx+1], np.random.random())
 
-    # find midpoint
-    midpoint = point_between(p_occl, p_disoccl, 0.5)
+    # pick random occlusion and disocclusion points lying on interpolated trajectory
+    p_occl = random_interpolated_point(past, (min_obs, past.shape[0]))
+    p_disoccl = random_interpolated_point(future, (0, future.shape[0]-min_reobs))
 
     # plot occlusion points
-    ax.scatter(past[past_idx][0], past[past_idx][1], marker="x", c="red")
-    ax.scatter(future[future_idx][0], future[future_idx][1], marker="x", c="orange")
     ax.scatter(p_occl[0], p_occl[1], marker="x", c="yellow")
     ax.scatter(p_disoccl[0], p_disoccl[1], marker="x", c="green")
-    ax.scatter(midpoint[0], midpoint[1], marker="x", c="blue")
 
     # generate safety buffer area around agent's trajectory
     r_agents = 60
-    too_close = LineString(np.concatenate((past, future), axis=0)).buffer(r_agents).convex_hull
+    target_buffer = LineString(np.concatenate((past, future), axis=0)).buffer(r_agents).convex_hull
 
-    # plot safety buffer
-    # plot_polygon(ax, too_close)
+    other_buffers = []
+    for other in range(len(instance_dict["agent_ids"])):
+        if instance_dict["agent_ids"][other] == agent_id:
+            continue
+        pst = instance_dict["pasts"][other]
+        ftr = instance_dict["futures"][other]
+        other_buffer = LineString(np.concatenate((pst, ftr), axis=0)).buffer(r_agents)
+        other_buffers.append(other_buffer)
 
     # set safety perimeter around the edges of the scene
     d_border = 120     # pixel distance from scene border
-    y_img, x_img = instance_dict["image_tensor"].shape[1:]
-
-    frame_box = Polygon([(0, 0),
-                         (x_img, 0),
-                         (x_img, y_img),
-                         (0, y_img),
-                         (0, 0)])
-
-    border_box = frame_box.buffer(-d_border)
-    no_ego_border = Polygon(shell=frame_box, holes=border_box)
-
-    # plot border box
-    # border_box_x, border_box_y = border_box.exterior.xy
-    # ax.plot(border_box_x, border_box_y, color="black", alpha=0.3)
+    frame_box = extruded_polygon(rectangle(instance_dict["image_tensor"]), d_border)
 
     # define no-ego regions based on taper angle, in order to prevent situations where agent is in direction of sight
     taper_angle = 60        # degrees
     u_traj = np.array(future[-1] - past[0])     # unit vector of agent's trajectory (future[-1], past[0])
-    u_traj = u_traj / np.linalg.norm(u_traj)
-    no_ego_1 = no_ego_cone(np.array(future[-1]), -u_traj, np.radians(taper_angle), frame_box)
-    no_ego_2 = no_ego_cone(np.array(past[0]), u_traj, np.radians(taper_angle), frame_box)
+    u_traj /= np.linalg.norm(u_traj)
+    no_ego_1 = bounded_wedge(np.array(future[-1]), -u_traj, float(np.radians(taper_angle)), rectangle(instance_dict["image_tensor"]))
+    no_ego_2 = bounded_wedge(np.array(past[0]), u_traj, float(np.radians(taper_angle)), rectangle(instance_dict["image_tensor"]))
+
+    no_egos = [no_ego_1, no_ego_2, target_buffer, frame_box]
+    no_egos.extend(other_buffers)
+
+    no_ego = unary_union(no_egos)
 
     # plot no-ego regions
-    # plot_polygon(ax, no_ego_1)
-    # plot_polygon(ax, no_ego_2)
-
-    nu = Polygon(frame_box.exterior.coords, [border_box.exterior.coords])
-    no_ego = unary_union((no_ego_1, no_ego_2, too_close, nu))
-
-    # plot_polygon(ax, no_ego, facecolor="red", edgecolor="red", alpha=0.2)
+    plot_polygon(ax, target_buffer, facecolor="red", alpha=0.2)
+    plot_polygon(ax, frame_box, facecolor="red", alpha=0.2)
+    plot_polygon(ax, no_ego_1, facecolor="red", alpha=0.2)
+    plot_polygon(ax, no_ego_2, facecolor="red", alpha=0.2)
+    [plot_polygon(ax, other_zone, facecolor="red", alpha=0.2) for other_zone in other_buffers]
 
     # extract polygons within which to sample our ego position
     yes_ego = [Polygon(hole) for hole in no_ego.interiors]
 
-    zone = yes_ego[0]
-    # add a hole to check.
-    # zone = Polygon(zone.exterior.coords, [Polygon([(1000, 250), (1300, 250), (1200, 400), (1000, 250)]).exterior.coords])
-    # plot_polygon(ax, zone, facecolor="blue", edgecolor="blue", alpha=0.2)
+    print(yes_ego)
+    yes_triangles = []
+    for zone in yes_ego:
+        yes_triangles.extend(polygon_triangulate(zone))
 
-    triangles = polygon_triangulate(zone)
-
-    for poly in triangles:
+    for poly in yes_triangles:
         plot_polygon(ax, poly, facecolor="green", edgecolor="green", alpha=0.2)
 
-    print(type(triangles))
+    ego_points = random_points_in_triangles_collection(yes_triangles, k=1)
 
-    points = random_points_in_triangles_collection(triangles, k=1000)
+    ax.scatter(ego_points[:, 0], ego_points[:, 1], marker="x", c="black")
 
-    for point in points:
-        ax.scatter(point[0], point[1])
+    # TODO
+    # WIP: generation of virtual occluding wall
+
+    for row_idx in range(ego_points.shape[0]):
+
+        wall_1 = point_between(ego_points[row_idx], np.array(p_occl), np.random.random())
+        wall_2 = point_between(ego_points[row_idx], np.array(p_disoccl), np.random.random())
+
+        ax.plot([wall_1[0], wall_2[0]], [wall_1[1], wall_2[1]], c="black")
 
     plt.show()
 
