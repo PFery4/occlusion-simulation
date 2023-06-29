dff4451c3e18d478c4e1e0cbd58177ac42120202
Paul Féry <paulfery4@gmail.com>
2023-06-29 16:12:58 +0200

    imitate signatures from agentformer implementation

--- src/data/agentformer_dataloader.py
+++ src/data/agentformer_dataloader.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+from io import TextIOWrapper
+
 # AgentFormer imports
 from utils.config import Config
 
@@ -7,7 +10,22 @@ class AgentFormerDataGenerator:
     This class wraps the dataset classes implemented in this repository in such a way that they are directly usable
     as 'generator' objects in the source code of AgentFormer: https://github.com/Khrylx/AgentFormer
     """
-    def __init__(self):
+    def __init__(self, parser: Config, log: TextIOWrapper, split: str = 'train', phase: str = 'training'):
+        pass
+
+    def shuffle(self) -> None:
+        pass
+
+    def get_seq_and_frame(self, index: int) -> Tuple[int, int]:
+        pass
+
+    def is_epoch_end(self) -> bool:
+        pass
+
+    def next_sample(self) -> dict:
+        pass
+
+    def __call__(self) -> dict:
         pass
 
 
