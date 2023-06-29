1d87b9450e1dd764e035fb601676e940fe450120
Paul Féry <paulfery4@gmail.com>
2023-06-29 14:29:59 +0200

    create file for the wrapper class for compatibility between own code and AgentFormer

---
+++ src/data/agentformer_dataloader.py
@@ -0,0 +1,13 @@
+
+
+class AgentFormerDataGenerator:
+    """
+    This class wraps the dataset classes implemented in this repository in such a way that they are directly usable
+    as 'generator' objects in the source code of AgentFormer: https://github.com/Khrylx/AgentFormer
+    """
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print("Hello World!")
