f2b6fbff87ceaa15e2b2d11193991335e62f5f2a
Paul Féry <paulfery4@gmail.com>
2023-07-13 16:14:37 +0200

    fix import statements

--- src/visualization/simulation_visualize.py
+++ src/visualization/simulation_visualize.py
@@ -1,47 +1,12 @@
 import matplotlib.pyplot as plt
 import matplotlib.axes
-import matplotlib.path as mpl_path
-import matplotlib.patches as mpl_patches
-import matplotlib.collections as mpl_coll
-import shapely.geometry as sp
 import skgeom as sg
 import numpy as np
-from typing import Union, List
+from typing import List
 import src.visualization.sdd_visualize as sdd_visualize
 from src.data.sdd_dataloader import StanfordDroneDataset
 from src.occlusion_simulation.simple_occlusion import simulate_occlusions
-
-
-def plot_sp_polygon(ax: matplotlib.axes.Axes, poly: sp.Polygon, **kwargs) -> None:
-    path = mpl_path.Path.make_compound_path(
-        mpl_path.Path(np.asarray(poly.exterior.coords)[:, :2]),
-        *[mpl_path.Path(np.asarray(ring.coords)[:, :2]) for ring in poly.interiors]
-    )
-
-    patch = mpl_patches.PathPatch(path, **kwargs)
-    collection = mpl_coll.PatchCollection([patch], **kwargs)
-
-    ax.add_collection(collection, autolim=True)
-    ax.autoscale_view()
-
-
-def plot_sg_polygon(ax: matplotlib.axes.Axes, poly: Union[sg.Polygon, sg.PolygonWithHoles], **kwargs) -> None:
-    if isinstance(poly, sg.Polygon):
-        path = mpl_path.Path(poly.coords)
-    elif isinstance(poly, sg.PolygonWithHoles):
-        path = mpl_path.Path.make_compound_path(
-            mpl_path.Path(poly.outer_boundary().coords),
-            *[mpl_path.Path(hole.coords) for hole in poly.holes]
-        )
-    else:
-        print(f"incorrect object type:\n{type(poly)}")
-        return None
-
-    patch = mpl_patches.PathPatch(path, **kwargs)
-    collection = mpl_coll.PatchCollection([patch], **kwargs)
-
-    ax.add_collection(collection, autolim=True)
-    ax.autoscale_view()
+from src.visualization.plot_utils import plot_sg_polygon
 
 
 def plot_simulation_step_1(
