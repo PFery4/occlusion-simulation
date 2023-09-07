f247942cd6f2966d5f868c7a0666cb2876def9ed
Paul Féry <paulfery4@gmail.com>
2023-09-07 10:37:25 +0200

    update comments of the get_config function

--- src/data/config.py
+++ src/data/config.py
@@ -6,22 +6,21 @@ REPO_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", 
 
 def get_config(config_filename) -> dict:
     """
-    reads the config file '{config_filename}.yaml' at the root of the project repository
+    reads the config file '{config_filename}.yaml' that is inside the 'config' directory.
     :return: the contents of the config file, as a dict
     """
-    # read config file
+    # define the config file path
     confpath = os.path.join(REPO_ROOT, "config", f"{config_filename}.yaml")
     assert os.path.exists(confpath), f"ERROR | PATH DOES NOT EXIST:\n{confpath}"
     # print(f"Loading config from:\n{confpath}\n")
 
+    # read config file
     with open(confpath, "r") as stream:
         try:
             config = yaml.safe_load(stream)
             # print(config)
         except yaml.YAMLError as exc:
             print(exc)
-    # perform input verifications
-    assert config["hyperparameters"]["other_agents"] in ("IN", "OUT")
     return config
 
 
