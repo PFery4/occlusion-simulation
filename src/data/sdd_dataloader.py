ad137dc99f961755dc5f8b4644899e12599f6799
Paul Féry <paulfery4@gmail.com>
2023-09-12 10:57:00 +0200

    mini commit: commenting out some of the visualization code

--- src/data/sdd_dataloader.py
+++ src/data/sdd_dataloader.py
@@ -369,15 +369,15 @@ if __name__ == '__main__':
         )
     ##################################################################################################################
 
-    idx_samples = [42578, 62777, 79908, 41933, 90340, 80810]
-    for idx in idx_samples:
-        fig, ax = plt.subplots(1, 1)
-        instance_dict = dataset.__getitem__(idx)
-
-        ax.title.set_text(idx)
-        sdd_visualize.visualize_training_instance(
-            draw_ax=ax, instance_dict=instance_dict
-        )
+    # idx_samples = [42578, 62777, 79908, 41933, 90340, 80810, 79397]
+    # for idx in idx_samples:
+    #     fig, ax = plt.subplots(1, 1)
+    #     instance_dict = dataset.__getitem__(idx)
+    #
+    #     ax.title.set_text(idx)
+    #     sdd_visualize.visualize_training_instance(
+    #         draw_ax=ax, instance_dict=instance_dict
+    #     )
 
     # print(dataset.frames.columns)
     #
