d2e8b09b4774f5b3688bcc45842c2096bcda486b
Paul Féry <paulfery4@gmail.com>
2023-09-16 17:25:12 +0200

    create function to generate image collection of simulated occlusions

--- src/visualization/simulation_visualize.py
+++ src/visualization/simulation_visualize.py
@@ -316,5 +316,101 @@ def show_simulation():
     visualize_random_simulation_samples(dataset, config["occlusion_simulator"], 2, 2)
 
 
+def save_simulation_cases():
+    import os
+    import logging
+    import src.data.config as conf
+    import src.visualization.sdd_visualize as sdd_visualize
+    from tqdm import tqdm
+
+    logger = logging.getLogger(__name__)
+    c_handler = logging.StreamHandler()
+    logger.addHandler(c_handler)
+
+    n_examples = 100
+    clear_figure_folder = True
+
+    config = conf.get_config("config")
+    dataset = StanfordDroneDataset(config_dict=config)
+
+    # defining path and creating it if it does not exit
+    save_path = os.path.join(conf.REPO_ROOT, config['results']['fig_path'], 'simulation_examples')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    # clearing figures
+    if clear_figure_folder:
+        for item in os.listdir(save_path):
+            if item.endswith(".png"):
+                print(f"Removing: {item}")
+                os.remove(os.path.join(save_path, item))
+
+    # sampling instances
+    instance_indices = np.sort(np.random.choice(len(dataset), size=n_examples, replace=False))
+    print(f"{instance_indices=}")
+
+    # setting up error counter
+    err_count = 0
+
+    for idx in tqdm(instance_indices):
+        print(f"{idx=}")
+        gs_kw = dict(wspace=0.0, hspace=0.0)
+        fig, ax = plt.subplots(gridspec_kw=gs_kw)
+        instance_dict = dataset.__getitem__(idx)
+
+        sdd_visualize.draw_map_numpy(draw_ax=ax, scene_image=instance_dict["scene_image"])
+        sdd_visualize.visualize_training_instance(draw_ax=ax, instance_dict=instance_dict)
+
+        img = instance_dict["scene_image"]
+        agents = instance_dict["agents"]
+        past_window = instance_dict["past_window"]
+        future_window = instance_dict["future_window"]
+
+        try:
+            simulation_dict = simulate_occlusions(
+                config=config["occlusion_simulator"],
+                image_res=tuple(img.shape[:2]),
+                agents=agents,
+                past_window=past_window,
+                future_window=future_window
+            )
+
+            occlusion_target_coords = simulation_dict["occlusion_target_coords"]
+            ego_point = simulation_dict["ego_point"]
+            occluders = simulation_dict["occluders"]
+            occluded_regions = simulation_dict["occluded_regions"]
+
+            # visualization part
+            p_occls = [coords[0] for coords in occlusion_target_coords]
+            p_disoccls = [coords[1] for coords in occlusion_target_coords]
+            p1s = [occluder[0] for occluder in occluders]
+            p2s = [occluder[1] for occluder in occluders]
+
+            plot_simulation_step_6(
+                ax=ax,
+                p_occls=p_occls,
+                p_disoccls=p_disoccls,
+                ego_point=ego_point,
+                p1s=p1s,
+                p2s=p2s,
+                occluded_regions=occluded_regions
+            )
+            ax.set_title(idx)
+
+            img_path = os.path.join(save_path, f"example_{idx}.png")
+            plt.savefig(fname=img_path, format='png', bbox_inches='tight', pad_inches=0)
+
+        except Exception as ex:
+            err_count += 1
+            logger.exception("\n\nSimulation Failed:\n")
+            ax.text(
+                sum(ax.get_xlim())/2, sum(ax.get_ylim())/2, "FAILED",
+                fontsize=20, c="red", horizontalalignment="center", verticalalignment="center"
+            )
+
+    print(f"TOTAL AMOUNT OF ERRORS: {err_count} / {n_examples}")
+    
+
 if __name__ == '__main__':
-    show_simulation()
+    # show_simulation()
+    save_simulation_cases()
