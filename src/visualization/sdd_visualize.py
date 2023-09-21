d0e57dee69946119494805ad5f0fce6fa4c1e047
Paul Féry <paulfery4@gmail.com>
2023-09-21 16:00:28 +0200

    create function for estimation of px/m conversion, and function for copying of reference images to a figures directory

--- src/visualization/sdd_visualize.py
+++ src/visualization/sdd_visualize.py
@@ -277,6 +277,74 @@ def visualize_full_trajectories_on_all_scenes():
                 plt.close()
 
 
+def avg_walking_speed_px():
+    from tqdm import tqdm
+    config_dict = conf.get_config("config")
+    data_path = config_dict["dataset"]["path"]
+    print(f"Extracting data from:\n{data_path}\n")
+
+    # individual agents' walking speeds in px/s
+    avg_speeds = []
+
+    for scene_name in tqdm(os.scandir(os.path.join(data_path, "annotations"))):
+        for video_name in os.scandir(os.path.realpath(scene_name)):
+            # print(f"{os.path.realpath(video_name)=}")
+
+            annot_file_path = os.path.join(os.path.realpath(video_name), "annotations.txt")
+            annot_file_df = sdd_data_processing.pd_df_from(annotation_filepath=annot_file_path)
+
+            annot_file_df = annot_file_df[annot_file_df['label'] == 'Pedestrian']
+            annot_file_df = sdd_data_processing.perform_preprocessing_pipeline(
+                annot_df=annot_file_df,
+                target_fps=2.5, orig_fps=30
+            )
+
+            agents = [StanfordDroneAgent(annot_file_df[annot_file_df["Id"] == agent_id])
+                      for agent_id in annot_file_df["Id"].unique()]
+
+            for agent in agents:
+                traj = agent.fulltraj
+                if len(traj) < 2:
+                    # print(f"CONTINUE")
+                    continue
+                velocity_profile = traj[1:] - traj[:-1]
+                speed_profile = np.linalg.norm(velocity_profile, axis=1)
+                avg_speed = np.mean(speed_profile)
+                avg_speeds.append(avg_speed)
+
+    plt.hist((np.array(avg_speeds)/2.5), bins=100)
+    print(f"{np.mean(avg_speeds)/2.5=} [px/s]")
+    print(f"{np.median(avg_speeds)/2.5=} [px/s]")
+
+    avg_walking_speed = 1.42    # [m/s]
+    avg_px_per_s = 5.3          # [px/s], reading from histogram
+
+    px_per_m = avg_px_per_s / avg_walking_speed
+
+    print(f"PIXELS PER METER: {px_per_m} [px/m]")       # this value seems inaccurate
+
+    # instead, we prefer to focus on using landmarks in the videos to measure distances in pixels, which we can
+    # compare to the real, physical distances that we can measure in the real world
+    # (physical measurements are obtained thanks to satellite collected geographic data)
+    measured_data = [
+        ['bookstore', 668.4, 25.3],
+        ['coupa', 814.3, 22.7],
+        ['deathCircle', 422.1, 17.47],
+        ['gates', 544.3, 20.88],
+        ['hyang', 933.0, 31.73],
+        ['little', 677.4, 19.81],
+        ['nexus', 783.0, 34.03],
+        ['quad', 820.1, 35.94],
+    ]
+    geographic_measurements = pd.DataFrame(measured_data, columns=['scene', 'px', 'm'])
+
+    geographic_measurements['px/m'] = geographic_measurements['px']/geographic_measurements['m']
+
+    print(f"{geographic_measurements=}")
+    print(f"{np.mean(geographic_measurements['px/m'])=}")
+    plt.show()
+
+
 def get_video_resolutions():
     config = conf.get_config("config")
     data_dir = os.path.join(config["dataset"]["path"], "annotations")
@@ -310,9 +378,30 @@ def get_video_resolutions():
     print(len(whds))
 
 
+def copy_reference_images_to_figures_dir():
+    import shutil
+    config_dict = conf.get_config("config")
+    data_path = config_dict["dataset"]["path"]
+    output_path = os.path.join(conf.REPO_ROOT, config_dict["results"]["fig_path"], "reference_images")
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for scene in os.scandir(os.path.join(data_path, "annotations")):
+        for video in os.scandir(os.path.realpath(scene)):
+
+            img_file = os.path.join(os.path.realpath(video), "reference.jpg")
+            assert os.path.exists(img_file)
+            out_file = os.path.join(output_path, f"{scene.name}_{video.name}.jpg")
+            shutil.copy(img_file, out_file)
+
+
 def main():
     # visualize_full_trajectories_on_all_scenes()
-    get_video_resolutions()
+    # get_video_resolutions()
+    avg_walking_speed_px()
+    # copy_reference_images_to_figures_dir()
+
 
 
 if __name__ == '__main__':
