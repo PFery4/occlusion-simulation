ea7365314db97a0510cc8f63cf7f0ced5d5fdfc6
Paul Féry <paulfery4@gmail.com>
2023-10-10 18:11:03 +0200

    update pixel/m conversion

--- src/data/config.py
+++ src/data/config.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import pandas as pd
 
 REPO_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
 # homography data: for each scene, we measure a distance in pixel space and real physical space.
@@ -9,17 +10,7 @@ REPO_ROOT = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", 
 # (this might not always be the case, as with deathCircle video3 for example, which is filming closer to the ground
 # than the other videos of this scene). The purpose of those measurements is to obtain a *rough* estimate for a
 # pixel/meter conversion
-# HOMOGRAPHY_DATA = {             # [px, m]
-#     'bookstore': [668.4, 25.3],
-#     'coupa': [814.3, 22.7],
-#     'deathCircle': [422.1, 17.47],
-#     'gates': [544.3, 20.88],
-#     'hyang': [933.0, 31.73],
-#     'little': [677.4, 19.81],
-#     'nexus': [783.0, 34.03],
-#     'quad': [820.1, 35.94],
-# }
-PX_PER_M = 27.74344283640459        # [px/m], the average px/m value across every scene in HOMOGRAPHY_DATA
+PX_PER_M = pd.read_csv(os.path.join(REPO_ROOT, "config", "pixel_to_meter.txt"), sep=", ", index_col=('scene', 'video'))
 
 
 def get_config(config_filename) -> dict:
